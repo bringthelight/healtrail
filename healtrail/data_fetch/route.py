befc0a5f40e5172050b87403ed9f04a1dce077e2
Prince Kumar <152162433+bringthelight@users.noreply.github.com>
2025-04-22 15:40:28 +0530

    2nd commit

--- healtrail/data_fetch/route.py
+++ healtrail/data_fetch/route.py
@@ -14,4 +14,19 @@ def fetch():
     data = cur.fetchall()
     return render_template('medicine_unit.html', data=data)
 
-@data_fetch.route()
+
+# add medicines
+
+
+@data_fetch.route('/add',methods=['GET','POST'])
+def add():
+    cur=mysql.connection.cursor()
+    if request.method=='POST':
+        unitname=request.form['medname']
+        shname=request.form['shortname']
+        cur=cur.execute("INSERT INTO master_unit (unit_name,unit_short_name) VALUES (%s, %s)", (unitname, shname,))
+        
+        mysql.connection.commit()
+        
+        return redirect(url_for('data_fetch.fetch'))
+        
