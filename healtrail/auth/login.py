5307a7ed7ebfdee088146fd7761ee578ed257af6
Prince Kumar <152162433+bringthelight@users.noreply.github.com>
2025-04-23 17:28:22 +0530

    changed gitignore and added session

--- healtrail/auth/login.py
+++ healtrail/auth/login.py
@@ -1,6 +1,7 @@
-from flask import request,url_for,redirect,render_template,session
+from flask import request,url_for,redirect,render_template,session,flash
 from flask import Blueprint
 from flask_mysqldb import MySQLdb,MySQL
+from flask_bcrypt import bcrypt
 
 
 
@@ -12,13 +13,14 @@ def login():
         email=request.form.get('email')
         password=request.form.get('password')
         cur=mysql.connection.cursor()
-        cur.execute("SELECT * FROM sign_up WHERE email = %s",(email,))
+        cur.execute("SELECT * FROM pharmacy_users WHERE email = %s",(email,))
         user=cur.fetchone()
-        # if user:
-        #     session['loggedin']=True
-        #     session['user']= user['email']
-        #     session['user_id']= user['id']
+        #print(bcrypt.generate_password_hash(password).decode('utf-8'))
+        if  user: # and bcrypt.check_password_hash(user['password'], password)
+            session['loggedin']=True
+            session['user']= user['email']
+            session['user_id']= user['id']
         return redirect(url_for('data_fetch.fetch'))
     else:
-        pass
+      flash('Incorrect email or Password ')  
     return render_template('sign-in.html')
